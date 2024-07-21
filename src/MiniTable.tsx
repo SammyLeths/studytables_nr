86efa13039834d009fbc88a80e44bfbcc0b8cbe9
SammyLeths <s.o.eyiowuawi@gmail.com>
2024-07-21 20:02:32 +0100

    disabled expandable rows

--- src/MiniTable.tsx
+++ src/MiniTable.tsx
@@ -61,7 +61,7 @@ const MiniTable = ({ data }: MiniTableProps) => {
                   >
                     {data.symbol}
                   </div>
-                  <div className="px-3" onClick={toggleRow}>
+                  <div className="px-3 hidden" onClick={toggleRow}>
                     {isOpen ? (
                       <ChevronUpIcon className="size-7" />
                     ) : (
@@ -71,115 +71,109 @@ const MiniTable = ({ data }: MiniTableProps) => {
                 </div>
               </td>
             </tr>
-            {isOpen && (
-              <>
-                <tr className="h-10">
-                  <th rowSpan={9}></th>
-                  <th className="px-4 border border-slate-200">Name</th>
-                  <td
-                    className={`px-4 border border-slate-200 cursor-pointer ${
-                      activeCells["name"] && `bg-gray-300`
-                    }`}
-                    onClick={() => toggleActive("name")}
-                  >
-                    {data.name}
-                  </td>
-                </tr>
-                <tr className="h-10">
-                  <th className="px-4 border border-slate-200">
-                    Price (Intraday)
-                  </th>
-                  <td
-                    className={`px-4 border border-slate-200 font-semibold cursor-pointer ${
-                      activeCells["price"] && `bg-gray-300`
-                    }`}
-                    onClick={() => toggleActive("price")}
-                  >
-                    {data.price}
-                  </td>
-                </tr>
-                <tr className="h-10">
-                  <th className="px-4 border border-slate-200">Change</th>
-                  <td
-                    className={`px-4 border border-slate-200 font-semibold cursor-pointer ${checkColor(
-                      data.change
-                    )} ${activeCells["change"] && `bg-gray-300`}`}
-                    onClick={() => toggleActive("change")}
-                  >
-                    {checkSign(data.change)}
-                  </td>
-                </tr>
-                <tr className="h-10">
-                  <th className="px-4 border border-slate-200">% Change</th>
-                  <td
-                    className={`px-4 border border-slate-200 font-semibold cursor-pointer ${checkColor(
-                      data.percent_change
-                    )} ${activeCells["percent_change"] && `bg-gray-300`}`}
-                    onClick={() => toggleActive("percent_change")}
-                  >
-                    {checkSign(data.percent_change) + "%"}
-                  </td>
-                </tr>
-                <tr className="h-10">
-                  <th className="px-4 border border-slate-200">Volume</th>
-                  <td
-                    className={`px-4 border border-slate-200 cursor-pointer ${
-                      activeCells["volume"] && `bg-gray-300`
-                    }`}
-                    onClick={() => toggleActive("volume")}
-                  >
-                    {numberFormatter(data.volume)}
-                  </td>
-                </tr>
-                <tr className="h-10">
-                  <th className="px-4 border border-slate-200">
-                    Avg Vol (3 months)
-                  </th>
-                  <td
-                    className={`px-4 border border-slate-200 cursor-pointer ${
-                      activeCells["average_volume"] && `bg-gray-300`
-                    }`}
-                    onClick={() => toggleActive("average_volume")}
-                  >
-                    {numberFormatter(data.average_volume)}
-                  </td>
-                </tr>
-                <tr className="h-10">
-                  <th className="px-4 border border-slate-200">Market Cap</th>
-                  <td
-                    className={`px-4 border border-slate-200 cursor-pointer ${
-                      activeCells["market_cap"] && `bg-gray-300`
-                    }`}
-                    onClick={() => toggleActive("market_cap")}
-                  >
-                    {numberFormatter(data.market_cap)}
-                  </td>
-                </tr>
-                <tr className="h-10">
-                  <th className="px-4 border border-slate-200">
-                    PE Ratio (TTM)
-                  </th>
-                  <td
-                    className={`px-4 border border-slate-200 cursor-pointer ${
-                      activeCells["pe_ratio"] && `bg-gray-300`
-                    }`}
-                    onClick={() => toggleActive("pe_ratio")}
-                  >
-                    {numberFormatter(data.pe_ratio)}
-                  </td>
-                </tr>
-                <tr className="h-10">
-                  <th className="px-4 border border-slate-200">
-                    52 Week Range
-                  </th>
-                  <RangeCell
-                    week_range={data.week_range}
-                    isActive={activeCells["week_range"]}
-                    toggleActive={() => toggleActive("week_range")}
-                  />
-                </tr>
-              </>
-            )}
+            {/* {isOpen && (
+              <> */}
+            <tr className="h-10">
+              <th rowSpan={9}></th>
+              <th className="px-4 border border-slate-200">Name</th>
+              <td
+                className={`px-4 border border-slate-200 cursor-pointer ${
+                  activeCells["name"] && `bg-gray-300`
+                }`}
+                onClick={() => toggleActive("name")}
+              >
+                {data.name}
+              </td>
+            </tr>
+            <tr className="h-10">
+              <th className="px-4 border border-slate-200">Price (Intraday)</th>
+              <td
+                className={`px-4 border border-slate-200 font-semibold cursor-pointer ${
+                  activeCells["price"] && `bg-gray-300`
+                }`}
+                onClick={() => toggleActive("price")}
+              >
+                {data.price}
+              </td>
+            </tr>
+            <tr className="h-10">
+              <th className="px-4 border border-slate-200">Change</th>
+              <td
+                className={`px-4 border border-slate-200 font-semibold cursor-pointer ${checkColor(
+                  data.change
+                )} ${activeCells["change"] && `bg-gray-300`}`}
+                onClick={() => toggleActive("change")}
+              >
+                {checkSign(data.change)}
+              </td>
+            </tr>
+            <tr className="h-10">
+              <th className="px-4 border border-slate-200">% Change</th>
+              <td
+                className={`px-4 border border-slate-200 font-semibold cursor-pointer ${checkColor(
+                  data.percent_change
+                )} ${activeCells["percent_change"] && `bg-gray-300`}`}
+                onClick={() => toggleActive("percent_change")}
+              >
+                {checkSign(data.percent_change) + "%"}
+              </td>
+            </tr>
+            <tr className="h-10">
+              <th className="px-4 border border-slate-200">Volume</th>
+              <td
+                className={`px-4 border border-slate-200 cursor-pointer ${
+                  activeCells["volume"] && `bg-gray-300`
+                }`}
+                onClick={() => toggleActive("volume")}
+              >
+                {numberFormatter(data.volume)}
+              </td>
+            </tr>
+            <tr className="h-10">
+              <th className="px-4 border border-slate-200">
+                Avg Vol (3 months)
+              </th>
+              <td
+                className={`px-4 border border-slate-200 cursor-pointer ${
+                  activeCells["average_volume"] && `bg-gray-300`
+                }`}
+                onClick={() => toggleActive("average_volume")}
+              >
+                {numberFormatter(data.average_volume)}
+              </td>
+            </tr>
+            <tr className="h-10">
+              <th className="px-4 border border-slate-200">Market Cap</th>
+              <td
+                className={`px-4 border border-slate-200 cursor-pointer ${
+                  activeCells["market_cap"] && `bg-gray-300`
+                }`}
+                onClick={() => toggleActive("market_cap")}
+              >
+                {numberFormatter(data.market_cap)}
+              </td>
+            </tr>
+            <tr className="h-10">
+              <th className="px-4 border border-slate-200">PE Ratio (TTM)</th>
+              <td
+                className={`px-4 border border-slate-200 cursor-pointer ${
+                  activeCells["pe_ratio"] && `bg-gray-300`
+                }`}
+                onClick={() => toggleActive("pe_ratio")}
+              >
+                {numberFormatter(data.pe_ratio)}
+              </td>
+            </tr>
+            <tr className="h-10">
+              <th className="px-4 border border-slate-200">52 Week Range</th>
+              <RangeCell
+                week_range={data.week_range}
+                isActive={activeCells["week_range"]}
+                toggleActive={() => toggleActive("week_range")}
+              />
+            </tr>
+            {/* </>
+            )} */}
           </tbody>
         </table>
       </td>
